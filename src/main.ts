dd178cc3c1e5501c7560756a8f8516d9ee9fa532
MicheleSaraceni <michele.saraceni23@email.com>
2025-06-04 18:58:20 +0200

    Milestone 2 ended

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,15 @@ type Person = {
   biography: string,
   image: string
 }
+
+/*
+awards: una stringa
+nationality: una stringa tra un insieme definito di valori.
+Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
+*/
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: "American" | "British" | "Australian" | "Israeli-American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese",
+}
