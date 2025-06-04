88c5b3682904f36fd62349a730498814b40295c3
MicheleSaraceni <michele.saraceni23@email.com>
2025-06-04 20:05:22 +0200

    Milestone 3 ended

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,61 @@ type Person = {
   image: string
 }
 
-/*
-awards: una stringa
-nationality: una stringa tra un insieme definito di valori.
-Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
-*/
-
 type Actress = Person & {
   most_famous_movies: [string, string, string],
   awards: string,
   nationality: "American" | "British" | "Australian" | "Israeli-American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese",
 }
+
+const nationalityArray: string[] = ["American", "British", "Australian", "Israeli-American", "South African", "French", "Indian", "Israeli", "Spanish", "South Korean", "Chinese"]
+
+
+
+const API_URL = "http://localhost:3333";
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const res = await fetch(`${API_URL}/actresses/:${id}`);
+    const dati: unknown = await res.json();
+    if (!isActress(dati)) {
+      throw new Error("Dati non validi")
+    }
+    return dati;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error)
+    }
+  }
+}
+
+
+
+function isActress(dati: unknown): dati is Actress {
+  return (
+    typeof dati === "object" &&
+    dati !== null &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    "death_year" in dati &&
+    typeof dati.death_year === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "most_famous_movies" in dati &&
+    Array.isArray(dati.most_famous_movies) &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every((film => typeof film === "string")) &&
+    "awards" in dati &&
+    typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string" &&
+    nationalityArray.includes(dati.nationality)
+  )
+}
+
+
